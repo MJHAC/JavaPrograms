e771f39a0fe980932019456f7df803c6670151bd
Mohamed Junaid <mohamedjunaid500@gmail.com>
2025-08-18 23:46:04 +0530

    Adding programs..... yuhu Patterns

---
+++ src/Pattern1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        int row,col;
+        char ch;
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the rows and columns to get a Pattern: ******** the same paatern repeats in all the rows");
+        row=obj.nextInt();
+        col= obj.nextInt();
+        System.out.println("Enter the character you want:");
+        ch=obj.next().charAt(0);
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                System.out.print(ch+"");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Pattern5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        int row;
+        char ch;
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the number of rows to get a Pattern: 12345 the pattern decreases by one until the last row and the number also decreases by one");
+        row=obj.nextInt();
+        for(int i=row;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+"");
+            }
+            System.out.println();
+        }
+    }
+}
