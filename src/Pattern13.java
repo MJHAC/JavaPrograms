75e4f384d789498de67e94b32aab83da718c0b09
Mohamed Junaid <mohamedjunaid500@gmail.com>
2025-08-19 22:22:56 +0530

    Adding programs..... yuhu more more Patterns

---
+++ src/Pattern13.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Pattern13 {
+    public static void main(String[] args) {
+        int row,col;
+        char ch;
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the number of rows and columns to get a Pattern:");
+        row=obj.nextInt();
+        col=obj.nextInt();
+        for(int i=row;i>=1;i--){
+            for(int j=1;j<=row-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print(i+"");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Pattern17.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pattern17 {
+    public static void main(String[] args) {
+        int row,col;
+        char ch;
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the number of rows and columns to get a Pattern:");
+        row=obj.nextInt();
+        col=obj.nextInt();
+        int count=1;
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+}
