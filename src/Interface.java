349296857f5e9926084bce2bf83699d8de176f16
Mohamed Junaid <mohamedjunaid500@gmail.com>
2025-08-21 22:58:34 +0530

    Adding programs..... yuhu Interface

--- src/Interface.java
+++ src/Interface.java
@@ -1,7 +1,12 @@
+//In interface there is no body for the methods
 interface Bicycle{
     int a=10;
     void applyBrakes(int decrement);
     void speedUp(int increment);
+    //This can be used by making use of default methods
+    default void speed(){
+        System.out.println("Hello the speed is good");
+    }
 }
 class AvonCycle implements Bicycle{
     void blowHorn(){
@@ -18,6 +23,7 @@ class AvonCycle implements Bicycle{
 public class Interface {
     public static void main(String[] args) {
         AvonCycle cycle=new AvonCycle();
+        cycle.speed();
         cycle.applyBrakes(1);
         System.out.println(cycle.a);
         //You can't alter the properties in interface
